31f6f585b417193ed522de5d61688b22bbfd9f8b
Andrea Bizzotto <bizz84@gmail.com>
2015-01-28 09:04:31 +0000

    Add MVCarouselCollectionView as framework

---
+++ MVCarouselCollectionView/MVCarouselCollectionView.h
@@ -0,0 +1,17 @@
+//
+//  MVCarouselCollectionView.h
+//  MVCarouselCollectionView
+//
+//  Created by Andrea Bizzotto on 28/01/2015.
+//  Copyright (c) 2015 Muse Visions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MVCarouselCollectionView.
+FOUNDATION_EXPORT double MVCarouselCollectionViewVersionNumber;
+
+//! Project version string for MVCarouselCollectionView.
+FOUNDATION_EXPORT const unsigned char MVCarouselCollectionViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MVCarouselCollectionView/PublicHeader.h>
